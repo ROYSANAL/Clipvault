9086bbd5e6f2a92a1d45c3d249daea85dc80de72
ROYSANAL <sanalroy12345@gmail.com>
2025-01-28 02:42:08 +0530

    added jwt bcrypt and video, user schema

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//NOTE: to accept Data in JSON and define its limit
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+//NOTE: to accept Data from URL and define its limit and extended means nested objects bhi allowed
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+//NOTE: this means what we can save some static files and images on our server only in our public folder
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
+dotenv.config({
+  path: "./env",
+});
+
+//NOTE: 2nd Approach
+connectDB()
+  .then(() => {
+
+    NOTE://ERROR k liye listen karo, before starting the server, app.on() means on error kya krna hai?!
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
+//NOTE: 1st approach IIFE, try catch async await
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR", error);
+//       throw error;
+//     });
+//   } catch (error) {
+//     console.error("ERROR", error);
+//     throw error;
+//   }
+
+//   app.listen(process.env.PORT, () => {
+//     console.log(`App is listening on port : ${process.env.PORT}`);
+//   });
+// })();
